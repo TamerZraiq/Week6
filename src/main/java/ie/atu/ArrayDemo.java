897d1701231595a5edd66f682724fae0ffc7c860
Tamer Zraiq <G00425053@atu.ie>
2023-11-06 16:22:51 +0000

    Added Array Demo class

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -2,32 +2,6 @@ package ie.atu;
 
 public class ArrayDemo {
     public static void main(String[] args){
-        int[] anArray;
-
-        anArray = new int[10];
-
-        anArray [0] = 100;
-        anArray [1] = 100;
-        anArray [2] = 100;
-        anArray [3] = 100;
-        anArray [4] = 100;
-        anArray [5] = 100;
-        anArray [6] = 100;
-        anArray [7] = 100;
-        anArray [8] = 100;
-        anArray [9] = 100;
-
-        System.out.println("Element at index 0: " + anArray[0]);
-        System.out.println("Element at index 0: " + anArray[1]);
-        System.out.println("Element at index 0: " + anArray[2]);
-        System.out.println("Element at index 0: " + anArray[3]);
-        System.out.println("Element at index 0: " + anArray[4]);
-        System.out.println("Element at index 0: " + anArray[5]);
-        System.out.println("Element at index 0: " + anArray[6]);
-        System.out.println("Element at index 0: " + anArray[7]);
-        System.out.println("Element at index 0: " + anArray[8]);
-        System.out.println("Element at index 0: " + anArray[9]);
 
     }
-
 }
